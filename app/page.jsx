e95055d70a770e72f7738fd8bda42878ff41faa9
meruim <akatsukisasori938@gmai.com>
2024-07-10 22:49:41 +0800

    Minor changes

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,16 @@
 "use client";
 
 import React from "react";
+import Link from "next/link";
 
 function page() {
   return (
     <div className="min-h-screen bg-customBg text-white ">
       <h1 className="text-center">Nothing Here!!</h1>
+      Go on{" "}
+      <Link href="/profitCalculator" className="btn">
+        Profit Calculator
+      </Link>
     </div>
   );
 }
